d13641ef42fef76f95066346f45387565d72d994
Sean Dougherty <sean@lupinehook.com>
2021-03-29 14:08:14 -0700

    updated readme with design spec info. made snake move to the right

--- README.md
+++ README.md
@@ -12,6 +12,29 @@ Blocks will either be open board, snake, or possibly collectible for points or f
 
 We will be keeping track of the snake as a list of different positions, or blocks.
 
+## Drawing and Alignment on the board
+When looking at the draw methods for the fruit and snake, the following design decision was landed on:
+
+The fruit and snake will be placed in a grid-like position as everything is based on the size of each cell position. So you take x/y vector position and multiply
+it by the size of each cell and you wind up in the corresponding placement on the board.  The rand is done with -1 on the offchance that something will randomly generate
+off the edge of the board when drawing the rects for the fruit
+
+Worth noting: pygame.Rect expects ints. when working with Vector we get floats. So cast to int making things explicit.
 
 ## Collectibles 
 Class-based. This will determine x/y position on the board, which will be random.
+
+### Snake
+This will work like the fruit except that we will store and draw multiple vectors vs. a single vector.
+
+#### Movement
+Snake is moved via user input. If no input is provided, it will move based on the last provided input (a straight line). 
+Basically we are going to take the stored list that contains every snake element location (vector2) sans the 
+last element, create a copy of that list, and add to the 0th element of that new structure, a head that is placed at the location of
+the 0th position of the original snake + the direction from the user input. 1,0 for instance. If the head is currently at 2,3, and we 
+move to the right, the new head position will be 2,3 + 1,0...new head location will be at 3,3. 
+
+we do not want the snake moving all the time, so we need a timer. let's set this to move every 150ms. So we will create a pygame USEREVENT
+and a pygame timer. When the timer reaches the defined amount of time, the custom event will be triggered. We can then use the same 
+pygame.event.get() approach in the main game loop to check if that event has be triggered. If he has, then we simply call the move_snake method.
+
--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import random
 import sys, pygame
+from pygame.math import Vector2
 from settings import cell_number, cell_size
 
 
@@ -8,14 +9,30 @@ class FRUIT:
     def __init__(self):
         self.x = random.randint(0, cell_number - 1)
         self.y = random.randint(0, cell_number - 1)
-        self.pos = pygame.math.Vector2(self.x, self.y)
+        self.pos = Vector2(self.x, self.y)
 
     def draw_fruit(self):
-        # this will place the fruit in a grid-like position as everything is based on the size of each cell
-        # position will be determined by the above vector2 x/y values
-        # Rect expects ints. when working with Vector we get floats. So cast to int making things explicit.
         fruit_rect = pygame.Rect(int(self.pos.x * cell_size), int(self.pos.y * cell_size), cell_size, cell_size)
         pygame.draw.rect(screen, (126, 166, 114), fruit_rect)
+
+
+class SNAKE:
+    def __init__(self):
+        # list containing all snake body parts as vectors
+        self.body = [Vector2(5, 10), Vector2(6, 10), Vector2(7, 10)]
+        self.direction = Vector2(1,0)
+
+    def draw_snake(self):
+        for block in self.body:
+            snake_body_rect = pygame.Rect(int(block.x * cell_size), int(block.y * cell_size), cell_size, cell_size)
+            pygame.draw.rect(screen, (255, 0, 0), snake_body_rect)
+
+    def move_snake(self):
+        body_double = self.body[:-1]
+        body_double.insert(0, body_double[0] + self.direction)
+        self.body = body_double[:]
+
+
 
 
 pygame.init()
@@ -23,13 +40,21 @@ screen = pygame.display.set_mode((cell_size * cell_number, cell_size * cell_numb
 clock = pygame.time.Clock()
 
 Orange = FRUIT()
+Snake = SNAKE()
+
+SNAKE_IS_MOVING = pygame.USEREVENT
+pygame.time.set_timer(SNAKE_IS_MOVING, 150)
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == SNAKE_IS_MOVING:
+            Snake.move_snake()
 
     screen.fill((175, 215, 70))
     Orange.draw_fruit()
+    Snake.draw_snake()
     pygame.display.update()
     clock.tick(60)
