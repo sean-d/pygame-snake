bc6f9fcb9774e3540900bf4cab6be80532718e91
Sean Dougherty <sean@lupinehook.com>
2021-03-29 19:04:10 -0700

    nuked pycache...because. updated readme as working. added fruit pickup, collision death with boundaries and snake. also points when collecting fruit and the snake grows each time the fruit is eaten.

--- README.md
+++ README.md
@@ -24,17 +24,37 @@ Worth noting: pygame.Rect expects ints. when working with Vector we get floats. 
 ## Collectibles 
 Class-based. This will determine x/y position on the board, which will be random.
 
-### Snake
+### Consuming 
+When the head of the snake is on the same space as the fruit, the snake_eats_fruit method will reposition the fruit (basically eating it and then placing a new one)
+and add a length to the snake. The extension will be added to the tail and not the head. This is so if the fruit is at the edge of the board and you eat it, the new
+head won't instantly be out of bounds. There is no way to cleanly add to the tail so there are times it will look disjointed. I rather a bad look than a poor function. 
+additionally, this will add to the score.
+
+## Snake
 This will work like the fruit except that we will store and draw multiple vectors vs. a single vector.
 
-#### Movement
+### Movement
 Snake is moved via user input. If no input is provided, it will move based on the last provided input (a straight line). 
 Basically we are going to take the stored list that contains every snake element location (vector2) sans the 
 last element, create a copy of that list, and add to the 0th element of that new structure, a head that is placed at the location of
 the 0th position of the original snake + the direction from the user input. 1,0 for instance. If the head is currently at 2,3, and we 
 move to the right, the new head position will be 2,3 + 1,0...new head location will be at 3,3. 
 
+The snake will not be able to move down while going up, up while going down, right while going left, and left while going right. Why?
+That would cause an insta-collision which would end the game. So no doing cheater-uturns. This is coded in the game loop under event.keys.
+
 we do not want the snake moving all the time, so we need a timer. let's set this to move every 150ms. So we will create a pygame USEREVENT
 and a pygame timer. When the timer reaches the defined amount of time, the custom event will be triggered. We can then use the same 
 pygame.event.get() approach in the main game loop to check if that event has be triggered. If he has, then we simply call the move_snake method.
 
+* K_UP = Vector2(0, -1): Up
+* K_DOWN = Vector2(0, 1): Down
+* K_LEFT = Vector2(-1,0): Left
+* K_RIGHT = Vector2(1,0): Right
+
+
+## Death
+This is simple. Do not hit yourself or a wall. 
+
+
+        # self.body = [Vector2(7, 10), Vector2(6, 10), Vector2(5, 10)]
--- game.py
+++ game.py
@@ -1,46 +1,93 @@
+import pygame
 import random
-import sys, pygame
+import sys
+
 from pygame.math import Vector2
+
 from settings import cell_number, cell_size
 
 
 class FRUIT:
 
     def __init__(self):
-        self.x = random.randint(0, cell_number - 1)
-        self.y = random.randint(0, cell_number - 1)
-        self.pos = Vector2(self.x, self.y)
+        self.x, self.y, self.pos = 0, 0, Vector2(0, 0)
+        self.new_fruit()
 
     def draw_fruit(self):
         fruit_rect = pygame.Rect(int(self.pos.x * cell_size), int(self.pos.y * cell_size), cell_size, cell_size)
         pygame.draw.rect(screen, (126, 166, 114), fruit_rect)
 
+    def new_fruit(self):
+        self.x = random.randint(0, cell_number - 1)
+        self.y = random.randint(0, cell_number - 1)
+        self.pos = Vector2(self.x, self.y)
+
 
 class SNAKE:
     def __init__(self):
-        # list containing all snake body parts as vectors
-        self.body = [Vector2(5, 10), Vector2(6, 10), Vector2(7, 10)]
-        self.direction = Vector2(1,0)
+        self.body = [Vector2(7, 10)]
+        self.direction = Vector2(1, 0)
+        self.score = 0
+
+    def add_block_to_snake(self):
+        if len(self.body) >= 2:
+            last_item = self.body[-1]
+        else:
+            last_item = self.body[:]
+        self.body.append(Vector2(last_item.x, last_item.y + 1))
 
     def draw_snake(self):
         for block in self.body:
             snake_body_rect = pygame.Rect(int(block.x * cell_size), int(block.y * cell_size), cell_size, cell_size)
             pygame.draw.rect(screen, (255, 0, 0), snake_body_rect)
+            print(len(self.body))
 
     def move_snake(self):
-        body_double = self.body[:-1]
+        if len(self.body) >= 2:
+            body_double = self.body[:-1]
+        else:
+            body_double = self.body[:]
         body_double.insert(0, body_double[0] + self.direction)
         self.body = body_double[:]
 
 
+class GAME:
+    def __init__(self):
+        self.snake = SNAKE()
+        self.fruit = FRUIT()
+
+    def check_game_over(self):
+        # if x/y in head vector is less than  or equal 0 (far left wall) or greater than 20 (far right wall)
+        if not 0 <= self.snake.body[0].x < cell_number or not 0 <= self.snake.body[0].y < cell_number:
+            self.game_over()
+        for block in self.snake.body[1:]:
+            if block == self.snake.body[0]:
+                self.game_over()
+
+    def draw_things(self):
+        self.fruit.draw_fruit()
+        self.snake.draw_snake()
+
+    def game_over(self):
+        pygame.quit()
+        sys.exit()
+
+    def snake_eats_fruit(self):
+        if self.fruit.pos == self.snake.body[0]:
+            self.snake.score += 1
+            self.snake.add_block_to_snake()
+            self.fruit.new_fruit()
+
+    def update(self):
+        self.snake.move_snake()
+        self.check_game_over()
+        self.snake_eats_fruit()
 
 
 pygame.init()
 screen = pygame.display.set_mode((cell_size * cell_number, cell_size * cell_number))
 clock = pygame.time.Clock()
-
-Orange = FRUIT()
-Snake = SNAKE()
+Game = GAME()
 
 SNAKE_IS_MOVING = pygame.USEREVENT
 pygame.time.set_timer(SNAKE_IS_MOVING, 150)
@@ -51,10 +98,22 @@ while True:
             pygame.quit()
             sys.exit()
         if event.type == SNAKE_IS_MOVING:
-            Snake.move_snake()
+            Game.update()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                if Game.snake.direction.y != 1:
+                    Game.snake.direction = Vector2(0, -1)
+            if event.key == pygame.K_DOWN:
+                if Game.snake.direction.y != -1:
+                    Game.snake.direction = Vector2(0, 1)
+            if event.key == pygame.K_LEFT:
+                if Game.snake.direction.x != 1:
+                    Game.snake.direction = Vector2(-1, 0)
+            if event.key == pygame.K_RIGHT:
+                if Game.snake.direction.x != -1:
+                    Game.snake.direction = Vector2(1, 0)
 
     screen.fill((175, 215, 70))
-    Orange.draw_fruit()
-    Snake.draw_snake()
+    Game.draw_things()
     pygame.display.update()
     clock.tick(60)
